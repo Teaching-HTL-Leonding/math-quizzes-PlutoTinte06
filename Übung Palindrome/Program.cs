a51b69a78c43e292f08a6ca68c0568e31295b65e
PlutoTinte06 <113624288+PlutoTinte06@users.noreply.github.com>
2022-12-11 16:32:02 +0100

    Add files via upload

---
+++ Übung Palindrome/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Type in a positive number: ");
+int number_begin = int.Parse(Console.ReadLine()!);
+int number_reverse = 0;
+int number = number_begin;
+
+while (number > 0)
+{
+    int rest = number % 10;
+    number = number / 10;
+    number_reverse = (number_reverse * 10) + rest;
+}
+
+if (number_reverse == number_begin) {Console.WriteLine("The number is a palindrome");}
+else if (number_reverse != number_begin) {Console.WriteLine("The number is not a palindrome");}
